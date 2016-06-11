80d8107650274dd7ac30a094f1f25fe923fc2d58
raphael-bos <raphael_bos@poli.ufrj.br>
2016-06-11 17:15:57 -0300

    Commit do Intelij

---
+++ Spark/src/main/scala-2.10/Spark/SparkServer.scala
@@ -0,0 +1,25 @@
+package Spark{
+
+  import spark.{Request, Response, Route}
+  import spark.Spark._
+
+  object SparkServer {
+
+    def main(args: Array[String]) {
+
+      port(8080)
+      staticFiles.location("main/resources/public")
+
+      get("hello", new Route {
+        def handle(request: Request, response: Response): AnyRef = {
+          "Hello!"
+        }
+      })
+
+      redirect.get("/", "Index.html")
+
+    }
+  }
+
+}
+
