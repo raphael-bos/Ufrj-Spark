2b9672cc0f41abd4fc7cb76cf1feb3f372095277
raphael-bos <raphael_bos@poli.ufrj.br>
2016-07-20 12:41:56 -0300

    Commit do Intellij

--- Spark/src/main/scala-2.10/Spark/SparkManager.scala
+++ Spark/src/main/scala-2.10/Spark/SparkManager.scala
@@ -11,7 +11,7 @@ object SparkManager {
 
   def buildConfigureSpark() = {
     sparkConf = new SparkConf().setAppName("ProjetoSpark")
-      .set("spark.executor.memory","512m")
+      .set("spark.executor.memory","400m")
       .set("spark.cassandra.connection.host", "52.67.88.91")
       //.setMaster("local[*]")
     context = new SparkContext(sparkConf)
